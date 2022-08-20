eaf1c2204033d3625c96d165bd7f3a669ca4e50b
Ishrat Jahan Mitu <74704975+Ishrat-123@users.noreply.github.com>
2022-08-21 04:45:46 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB:
+  'mongodb+srv://classed:4zBJiRAj4USB6IzY@cluster0.ddjx4.mongodb.net/MongoData?retryWrites=true&w=majority'
+  ,
+  SECRET_KEY: 'some very secret key'
+};
