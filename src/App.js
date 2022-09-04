1e58eff23a8fea3e5262d16055a48d35492cca5f
Ishrat Jahan Mitu <74704975+Ishrat-123@users.noreply.github.com>
2022-09-04 21:48:43 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+
+import { AuthProvider } from './context/auth';
+import AuthRoute from './util/AuthRoute';
+
+import MenuBar from './Controller/MenuBar';
+import Home from './View/Home';
+import Login from './View/Login';
+import Register from './View/Register';
+import SinglePost from './View/SinglePost';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+          <Route exact path="/posts/:postId" component={SinglePost} />
+        </Container>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
